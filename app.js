5ff8e2fe08fc8e3e504ee03bada31288352cdab9
José Pereira Macedo <arseniumgx@gmail.com>
2025-02-20 19:45:59 -0300

    feat: aplicação parcialmente finalizada

--- app.js
+++ app.js
@@ -1,1 +1,62 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+const listaAmigosElement = document.querySelector("#listaAmigos");
+const amigoElement = document.querySelector("#amigo");
+const resultadoElement = document.querySelector("#resultado");
+
+let amigos = [];
+
+function adicionarAmigo() {
+  const amigo = amigoElement.value.trim();
+
+  if (checaSeNomeDigitado(amigo)) {
+    alert("Por favor, insira um nome.");
+    return;
+  }
+
+  const amigoJaNaLista = amigos.find(
+    (a) => a.toLowerCase() === amigo.toLowerCase()
+  );
+
+  if (amigoJaNaLista) {
+    alert("Amigo digitado já está na lista");
+    return;
+  }
+
+  amigos.push(amigo);
+  atualizaListaAmigos();
+  amigoElement.value = "";
+  amigoElement.focus();
+}
+
+function checaSeNomeDigitado(str) {
+  return str === "";
+}
+
+function atualizaListaAmigos() {
+  const amigoLi = document.createElement("li");
+  amigos.forEach((amigo) => {
+    amigoLi.classList.add("result-list");
+    amigoLi.innerHTML = amigo;
+  });
+  listaAmigosElement.appendChild(amigoLi);
+}
+
+function sortearAmigo() {
+  if (checaSeListaAmigosVazia()) {
+    alert("Não há amigos para a serem soteados");
+    return;
+  }
+
+  const resultListElement = document.querySelectorAll(".result-list");
+  resultListElement.forEach((element) => {
+    if (!element.id) element.remove();
+  });
+
+  const amigoSorteado = amigos[Math.floor(Math.random() * amigos.length)];
+  amigos = amigos.filter((amigo) => amigo !== amigoSorteado);
+
+  resultadoElement.innerHTML = amigoSorteado;
+}
+
+function checaSeListaAmigosVazia() {
+  return amigos.length === 0;
+}
